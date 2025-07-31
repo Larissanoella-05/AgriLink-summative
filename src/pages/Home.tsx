67cd7fe7345af36e2ab5960ad56f1669031a8453
larissanoellaa-05 <s.noella@alustudent.com>
2025-07-31 11:43:03 +0200

    chore: modified the home.tsx file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import HomeCrops from "@/features/Crops/HomeCrops"
 import HomeRecentCrops from "@/features/Crops/HomeRecentCrops"
 import About from "@/UI/About"
-import LandingCarousel from "@/UI/LandingCarousel"
+import AgricultureShowcase from "@/UI/AgricultureShowcase"
 
 export default function Home() {
   return (
     <div className="bg-background">
       <section id="home">
-        <LandingCarousel></LandingCarousel>
+        <AgricultureShowcase></AgricultureShowcase>
       </section>
       <section id="about">
         <About></About>
