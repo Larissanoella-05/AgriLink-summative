adc9a5113b6a0a98b095f9eb1d150120723774af
larissanoellaa-05 <s.noella@alustudent.com>
2025-07-31 12:05:33 +0200

    chore: Modified products.tsx file , removed unnecessary comments

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -14,15 +14,15 @@ export default function Products() {
 
   return (
     <div className="min-h-screen bg-background">
-      {/* Simple Container */}
+      {}
       <div className="container mx-auto px-4 py-8">
-        {/* Search Bar */}
+        {}
         <div className="mb-8">
           <ProductsSearch searchQuery={searchQuery} onSearchChange={setSearchQuery} />
         </div>
 
         <div className="flex flex-col lg:flex-row gap-8">
-          {/* Filters Sidebar */}
+          {}
           <div className="lg:w-1/4">
             <ProductsFilters
               selectedCategories={selectedCategories}
@@ -32,14 +32,14 @@ export default function Products() {
             />
           </div>
 
-          {/* Main Content */}
+          {}
           <div className="lg:w-3/4">
-            {/* Sort Options */}
+            {}
             <div className="mb-6">
               <ProductsSort sortBy={sortBy} onSortChange={setSortBy} />
             </div>
 
-            {/* Products Grid */}
+            {}
             <ProductsCrops
               searchQuery={searchQuery}
               selectedCategories={selectedCategories}
